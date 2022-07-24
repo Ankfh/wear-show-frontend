29c5aa7c80429b0681a881464b61f0bce8f80ec2
Ankfh <ashfaqnabi357@gmail.com>
2022-07-24 10:05:21 +0500

    commit by ashfaq first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="">
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <a className="brand" href="/">
+                WEARSHOW
+              </a>
+            </div>
+            <div>
+              <a href="/cart">Cart</a>
+              <a href="/signin">Sign In</a>
+            </div>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" component={HomeScreen}></Route>
+              <Route path="/product/:_id" component={ProductScreen}></Route>
+            </Routes>
+          </main>
+          <footer className="row center">All right reserved</footer>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
